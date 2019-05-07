a828d7ca729e5e24376f7449df6e8ccdca4f63f3
natberland <45376301+natberland@users.noreply.github.com>
2019-05-06 22:55:00 -0300

    Aula 1 - React

---
+++ src/pages/Product/index.jsx
@@ -0,0 +1,17 @@
+import React,  { Component } from 'react';
+
+class Product extends Component {
+    constructor(props) {
+        super(props);
+
+        console.log(props.match.params.id);
+    }
+
+    render() {
+        return (
+            <div>Product</div>
+        );
+    }
+}
+
+export default Product;
