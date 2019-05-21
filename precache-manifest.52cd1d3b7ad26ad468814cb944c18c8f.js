c48e1cdfacc59b4ee1f26c5aaa0cfd08a4fdce32
natberland <45376301+natberland@users.noreply.github.com>
2019-05-20 22:22:47 -0300

    build pwa
    pwa aula

---
+++ precache-manifest.52cd1d3b7ad26ad468814cb944c18c8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6682f777aa6f641de0c1ccf2e2d2043",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "d19561b1ddff06d4ec33",
+    "url": "/react/static/css/main.d56cb673.chunk.css"
+  },
+  {
+    "revision": "7470a9cbe7ba834875ef",
+    "url": "/react/static/js/2.bd61d7b2.chunk.js"
+  },
+  {
+    "revision": "d19561b1ddff06d4ec33",
+    "url": "/react/static/js/main.caf7f7bd.chunk.js"
+  },
+  {
+    "revision": "4e6a4b677d1c1e6ba359",
+    "url": "/react/static/js/runtime~main.bb50a20d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
