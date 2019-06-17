cb86d9c30af5222a6440b53ac9288ba66b89fd02
natberland <45376301+natberland@users.noreply.github.com>
2019-06-17 17:42:29 -0300

    Add files via upload

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -3,6 +3,9 @@ import React, { Component, Container, Row } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
+
+import * as CurrencyFormat from 'react-currency-format';
+
 class Search extends Component {
     constructor() {
         super();
@@ -27,25 +30,42 @@ class Search extends Component {
 
     renderItem(item) {
         return (
-            <div class="lista mdl-grid-2 ">
+            <div class="lista mdl-grid-2 mdl-cell--12-col-desktop">
             
             <li  key={ item.id }>
-                <div class=" mdl-cell--6-col">
-                <div><img class="thumb" src={item.thumbnail}></img></div>
+                <div  align="center">
+                    <div><img class="thumb" src={item.thumbnail}></img></div>
                 </div>
                 
-                <div class="mdl-cell mdl-cell--6-col">
-                <div class="prodtitle">{ item.title }</div>
+                <div align="center">
+                    <div class="id">{ item.id } </div>
+                    <div class="prodtitle">{ item.title } </div>
+                </div>
+
+                <div class="price-item">
+                     <h4 align="center">
+                         <CurrencyFormat value={item.price} 
+                                         decimalScale={2} 
+                                         thousandSeparator={"."} 
+                                         fixedDecimalScale={true} 
+                                         displayType={'text'}
+                                         decimalSeparator={","} 
+                                         thousandSpacing={2} 
+                                         prefix={'R$'} />
+                    </h4>                              
+                 </div>
                 
-                <div class="botao"><Link 
-                    to={ `/product/${item.id}` }
-                    >
+                <div>
 
-                Abrir Produto
+                    <div class="botao"><Link 
+                         to={ `/product/${item.id}` }>
+
+                         Abrir Produto
                  
-                </Link>
+                        </Link>
 
-                </div> 
+                    </div> 
+                
                 </div>
                 
             </li>
@@ -71,9 +91,17 @@ class Search extends Component {
                 <ul>
                     { this.state.results.map(this.renderItem) }
                 </ul>
+
+                
             </div>
+
+
+
+
         );
     }
 }
+
+
 
 export default Search;
